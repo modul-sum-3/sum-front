00a611074b7582da35f2c517460ea06211bf7502
Filip Warchoł <WarcholF@wp.pl>
2023-03-26 21:19:39 +0200

    initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import { Avatar } from "flowbite-react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex flex-wrap justify-center">
+        <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" />
+      </div>
+    </div>
+  );
+}
+
+export default App;

0c46187b811f7bf51a774a76a83569dd29aad886
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-04-14 13:27:14 +0200

    Move PassCard content to Pass

--- src/components/PassCard.js
+++ src/components/PassCard.js
@@ -1,4 +1,4 @@
-const PassCard = ({ title, price }) => {
+const PassCard = ({ children, title, price }) => {
   return (
     <div>
       <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800 sm:p-8">
@@ -16,140 +16,9 @@ const PassCard = ({ title, price }) => {
         </div>
 
         <ul role="list" className="my-7 space-y-5">
-          <li className="flex space-x-3">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              2 team members
-            </span>
-          </li>
-          <li className="flex space-x-3">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              20GB Cloud storage
-            </span>
-          </li>
-          <li className="flex space-x-3">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-              Integration help
-            </span>
-          </li>
-          <li className="flex space-x-3 line-through decoration-gray-500">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-gray-400 dark:text-gray-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500">
-              Sketch Files
-            </span>
-          </li>
-          <li className="flex space-x-3 line-through decoration-gray-500">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-gray-400 dark:text-gray-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500">
-              API Access
-            </span>
-          </li>
-          <li className="flex space-x-3 line-through decoration-gray-500">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-gray-400 dark:text-gray-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500">
-              Complete documentation
-            </span>
-          </li>
-          <li className="flex space-x-3 line-through decoration-gray-500">
-            <svg
-              aria-hidden="true"
-              className="h-5 w-5 flex-shrink-0 text-gray-400 dark:text-gray-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Check icon</title>
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span className="text-base font-normal leading-tight text-gray-500">
-              24×7 phone & email support
-            </span>
-          </li>
+          {children}
         </ul>
+
         <button
           type="button"
           className="inline-flex w-full justify-center rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-100 focus:outline-none focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"

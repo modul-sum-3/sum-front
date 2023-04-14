c9bace430957ba86681b5522af1be29afae3f0c8
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-04-14 19:49:43 +0200

    Modify Pass, add cards

--- src/components/PassCard.js
+++ src/components/PassCard.js
@@ -1,23 +1,35 @@
-const PassCard = ({ children, title, price }) => {
-  return (
-    <div>
-      <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800 sm:p-8">
-        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{title}</h5>
-        <div className="flex items-baseline text-gray-900 dark:text-white">
-          <span className="text-3xl font-semibold">$</span>
-          <span className="text-5xl font-extrabold tracking-tight">{price}</span>
-          <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
-        </div>
-        <ul className="my-7 space-y-5">{children}</ul>
-        <button
-          type="button"
-          className="inline-flex w-full justify-center rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-100 focus:outline-none focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
-        >
-          Choose plan
-        </button>
+import clsx from 'clsx';
+import { ReactComponent as CheckIcon } from '../assets/check-icon.svg';
+
+const PassCard = ({ title, price, benefits }) => (
+  <div>
+    <div className="w-full max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow dark:border-gray-700 dark:bg-gray-800 sm:p-8">
+      <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{title}</h5>
+      <div className="flex items-baseline text-gray-900 dark:text-white">
+        <span className="text-3xl font-semibold">$</span>
+        <span className="text-5xl font-extrabold tracking-tight">{price}</span>
+        <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
       </div>
+      <ul className="my-7 space-y-5">
+        {benefits.map(({ text, crossed }) => (
+          <li className="flex space-x-3">
+            <CheckIcon
+              className={clsx('h-5 w-5 shrink-0', crossed ? 'text-primary' : 'text-gray-500')}
+            />
+            <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
+              {text}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        className="inline-flex w-full justify-center rounded-lg bg-primary px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-100 focus:outline-none focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900"
+      >
+        Choose plan
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 export default PassCard;
---
+++ src/data/cards.js
@@ -0,0 +1,74 @@
+export const cards = [
+  {
+    title: 'Tytuł 1',
+    price: 10,
+    benefits: [
+      {
+        crossed: false,
+        text: 'benefit 1',
+      },
+      {
+        crossed: false,
+        text: 'benefit 2',
+      },
+      {
+        crossed: true,
+        text: 'benefit 3',
+      },
+    ],
+  },
+  {
+    title: 'Tytuł 2',
+    price: 20,
+    benefits: [
+      {
+        crossed: false,
+        text: 'benefit 1',
+      },
+      {
+        crossed: false,
+        text: 'benefit 2',
+      },
+      {
+        crossed: true,
+        text: 'benefit 3',
+      },
+    ],
+  },
+  {
+    title: 'Tytuł 3',
+    price: 30,
+    benefits: [
+      {
+        crossed: false,
+        text: 'benefit 1',
+      },
+      {
+        crossed: false,
+        text: 'benefit 2',
+      },
+      {
+        crossed: true,
+        text: 'benefit 3',
+      },
+    ],
+  },
+  {
+    title: 'Tytuł 4',
+    price: 40,
+    benefits: [
+      {
+        crossed: false,
+        text: 'benefit 1',
+      },
+      {
+        crossed: false,
+        text: 'benefit 2',
+      },
+      {
+        crossed: true,
+        text: 'benefit 3',
+      },
+    ],
+  },
+];

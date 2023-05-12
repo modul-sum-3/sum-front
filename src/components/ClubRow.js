281936d4d875cf885e2f63cc8dd34551aa67f886
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-05-12 09:38:46 +0200

    create working accordion

--- src/components/ClubRow.js
+++ src/components/ClubRow.js
@@ -5,44 +5,61 @@ import { ReactComponent as ArrowIcon } from '../assets/arrow-icon.svg';
 
 const ClubRow = ({ id, city, location, street, zip }) => {
   const [show, setShow] = useState(false);
-  const accordionClick = () => {};
+
+  const accordionToggle = () => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  };
   return (
     <div className="gap-0">
-      <div className="w-full rounded-t-lg border-gray-200 bg-white shadow hover:bg-gray-50">
-        <Link
+      <div className="flex w-full justify-between rounded-t-lg border-gray-200 bg-white shadow hover:bg-gray-50">
+        {/* <Link
           to={`/${id}`}
-          className="clubs__list__row flex  justify-between whitespace-nowrap p-6"
+          
+        > */}
+        <button
+          type="button"
+          onClick={accordionToggle}
+          className="flex w-full items-center justify-between rounded-t-lg border-gray-200 bg-white p-0 align-middle shadow hover:bg-gray-50"
         >
-          <div>
-            <h2 className="w-[400px] self-center font-semibold text-gray-600">
+          <div className="clubs__list__row flex justify-between whitespace-nowrap p-6">
+            <h2 className="w-[400px] self-center font-semibold text-gray-600 ">
               FitNest Club, {city}
             </h2>
           </div>
 
           <div className="flex w-[575px] gap-8">
-            <h3 className="w-[100px] self-center font-semibold text-gray-600">{location}</h3>
+            <h3 className="w-[110px] self-center font-semibold text-gray-600">{location}</h3>
             <p className="self-center text-gray-600">
               ul. {street}, {zip}
             </p>
           </div>
 
           {/* <p className="self-center text-gray-600">Click to expand</p> */}
-          <div className="self-center">
+          <div className="self-center p-6">
             <ArrowIcon className={clsx('h-5 w-5 shrink-0 fill-primary')} />
           </div>
-        </Link>
+        </button>
+        {/* </Link> */}
       </div>
 
       {/* accordion */}
-      <div className="w-full rounded-b-lg border-gray-200 bg-white shadow hover:bg-gray-50">
-        <div className="flex w-[575px] gap-8">
-          <h3 className="w-[100px] self-center font-semibold text-gray-600">{location}</h3>
-          <p className="self-center text-gray-600">
-            ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul
-            ululul ululul ululul ululul
-          </p>
-        </div>
-      </div>
+      {show && (
+        <Link to={`/${id}`}>
+          <div className="w-full rounded-b-lg border-gray-200 bg-white shadow hover:bg-gray-50">
+            <div className="flex w-[575px] gap-8">
+              <h3 className="w-[100px] self-center font-semibold text-gray-600">{location}</h3>
+              <p className="self-center text-gray-600">
+                ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul ululul
+                ululul ululul ululul ululul
+              </p>
+            </div>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };

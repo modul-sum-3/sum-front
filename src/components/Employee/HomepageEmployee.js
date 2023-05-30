afe02df1b526f61b724b1e68f42ef24cf40a5ed2
Filip Warchoł <WarcholF@wp.pl>
2023-05-31 01:20:03 +0200

    Employee page - homepage

--- src/components/Employee/HomepageEmployee.js
+++ src/components/Employee/HomepageEmployee.js
@@ -6,6 +6,27 @@ const HomepageEmployee = () => {
   const [clientId, setClientId] = useState('');
   const [user, setUser] = useState({});
   const [display, setDisplay] = useState('');
+  const [displayVisit, setDisplayVisit] = useState('block');
+  const [displayVisit2, setDisplayVisit2] = useState('hidden');
+
+  const isCarnetActive = false;
+
+  const buttonStart = document.getElementById('button-start');
+  const buttonFinish = document.getElementById('button-end');
+
+  useEffect(() => {
+    if (buttonStart || buttonFinish) {
+      buttonStart.addEventListener('click', () => {
+        setDisplayVisit('hidden');
+        setDisplayVisit2('block');
+      });
+
+      buttonFinish.addEventListener('click', () => {
+        setDisplayVisit('block');
+        setDisplayVisit2('hidden');
+      });
+    }
+  }, [buttonFinish, buttonStart]);
 
   useEffect(() => {
     if (clientId.length === 0) {
@@ -45,19 +66,66 @@ const HomepageEmployee = () => {
           Search
         </button>
       </div>
-      <div className="mt-10 flex h-3/4 justify-center">
+      <div className="mt-10 flex h-2/3 justify-center">
         <div
           className={`${display} block w-3/4 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary focus:ring-primary disabled:cursor-not-allowed disabled:opacity-50`}
         >
-          First name: {user.first_name}
-          <br />
-          Last name: {user.last_name}
-          <br />
-          Birthday: {user.date_of_birth}
-          <br />
-          Phone: {user.phoneNumber}
-          <br />
-          Email: {user.email}
+          <div className="flex flex-col">
+            <p className="mt-10 text-center text-2xl">Client information</p>
+            <div className="mt-6 flex flex-row items-center justify-center gap-7">
+              <section className="flex w-fit flex-col rounded-2xl border border-red-500 bg-gray-50 p-2.5 text-sm">
+                <div className=" ">First name: {user.first_name}</div>
+                <div className=" ">Last name: {user.last_name}</div>
+                <div>Birthday: {user.date_of_birth}</div>
+                <div>Phone: {user.phoneNumber}</div>
+                <div>Email: {user.email}</div>
+              </section>
+              <section className="flex w-fit flex-col rounded-2xl border border-red-500 bg-gray-50 p-2.5 text-sm">
+                <div>Current carnet:</div>
+                <div>Date of purchase:</div>
+                <div>Date of expiration:</div>
+                {isCarnetActive === false ? (
+                  <button
+                    type="button"
+                    className="mt-3 rounded-lg bg-red-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-red-500"
+                  >
+                    Activate carnet
+                  </button>
+                ) : null}
+              </section>
+            </div>
+            <p className="mt-10 text-center text-2xl">Visit information</p>
+            <div className="mt-6  flex items-center justify-center">
+              <div className="block w-3/4 ">
+                <section className="flex flex-col">
+                  <button
+                    id="button-start"
+                    type="button"
+                    className={`${displayVisit} mt-3 rounded-lg bg-red-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-red-500`}
+                  >
+                    Start Visit
+                  </button>
+
+                  <div className={`${displayVisit2}`}>
+                    <div className=" flex-col rounded-2xl border border-red-500 bg-gray-50 p-2.5 text-sm">
+                      <div>Visit start date:</div>
+                      <div>
+                        <p>Is on training:</p>
+                        <p>Room:</p>
+                      </div>
+                      <button
+                        id="button-end"
+                        type="button"
+                        className="mt-3 w-full rounded-lg bg-red-600 px-5 py-1.5 text-center text-sm font-medium text-white hover:bg-red-500"
+                      >
+                        Finish Visit
+                      </button>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

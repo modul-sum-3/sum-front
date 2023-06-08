8cfa0f37874e49ef94cd433ac14dbcad7a14acee
Kinga Wawrzyńczak <wawrzynczakkinga@gmail.com>
2023-06-08 18:57:02 +0200

    add borders bc it looks boring

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -47,14 +47,14 @@ const Contact = () => {
     <MainTemplate>
       <div className="absolute left-1/2 top-1/2 flex w-full max-w-screen-xl -translate-x-1/2 -translate-y-1/2 justify-between">
         <div className="flex flex-col gap-10">
-          <article>
+          <article className="rounded-xl border-[3px] border-primary">
             <div className="flex items-start justify-between rounded-t-lg border-b bg-white p-6">
-              <h2 className="font-semibold text-primary">Customer Service Department</h2>
+              <h2 className="font-semibold text-gray-800">Customer Service Department</h2>
             </div>
-            <div className="bg-white p-6 leading-8">
+            <div className="bg-white p-6 text-lg leading-8">
               <p className="text-gray-900">12 345 67 89 </p>
               <p>
-                <span className="font-semibold">contact@fitNest.bestrong.pl</span>
+                <span className="font-semibold text-primary">contact@fitNest.bestrong.pl</span>
               </p>
               <p className="text-gray-900">
                 We are working since Monday to Sunday, from 8:00 to 20:00.
@@ -63,25 +63,25 @@ const Contact = () => {
             <div className="flex items-center space-x-2 rounded-b-lg border-t border-gray-200 bg-white p-6" />
           </article>
 
-          <article>
+          <article className="rounded-xl border-[3px] border-primary">
             <div className="flex items-start justify-between rounded-t-lg border-b bg-white p-6">
-              <h2 className="font-semibold text-primary">Contact For Companies</h2>
+              <h2 className="font-semibold text-gray-800">Contact For Companies</h2>
             </div>
-            <div className="bg-white p-6 leading-8">
+            <div className="bg-white p-6 text-lg leading-8">
               <p className="text-gray-900">
                 Interested companies can send offers to the following e-mail address:
               </p>
               <p className="">
-                <span className="font-semibold">companies@fitNest.bestrong.pl</span>
+                <span className="font-semibold text-primary">companies@fitNest.bestrong.pl</span>
               </p>
             </div>
             <div className="flex items-center space-x-2 rounded-b-lg border-t border-gray-200 bg-white p-6" />
           </article>
         </div>
 
-        <div>
+        <div className="rounded-xl border-[3px] border-primary">
           <div className="flex items-start justify-between rounded-t-lg border-b bg-white p-6">
-            <h2 className="font-semibold text-primary">Send us a message</h2>
+            <h2 className="font-semibold text-gray-800">Send us a message</h2>
           </div>
           <div className="bg-white p-12">
             <form className="flex flex-col gap-8 max-lg:w-1/2 max-md:w-2/3" onSubmit={handleSend}>
